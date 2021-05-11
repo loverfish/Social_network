79136cbad7d7179286e0e186ae819bfd28985d0e
loverfish <blackmemberb@gmail.com>
2021-05-12 00:28:17 +0300

    Added tests for Follows, Comments

--- yatube/posts/tests.py
+++ yatube/posts/tests.py
@@ -2,14 +2,120 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from django.core.cache import cache
 
-from .models import Group, Post, User
+from .models import Group, Post, User, Follow
 
 
-class ErrorTest(TestCase):
-    def test_404_error(self):
-        response = self.client.get('fgjsfg')
-        self.assertEqual(response.status_code, 404)
-        self.assertTemplateUsed(response, template_name='misc/404.html')
+class NewPostTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', email='q@q.com', password='12345')
+        self.client.login(username='testuser', password='12345')
+        self.group = Group.objects.create(title='test', slug='test', description='empty')
+        self.client.post(reverse('new_post'), data={
+            'author': self.user, "group": self.group.id, "text": "New_post"}, follow=True)
+
+    def test_auth_user_could_add_post(self):
+        """Авторизированный пользователь может создавать посты, сохраняются в бд"""
+        response = self.client.get(reverse('new_post'))
+        self.assertFalse(Post.objects.filter(text='test_post'))
+        self.assertEqual(response.status_code, 200)
+        self.client.post(reverse('new_post'), data={'text': 'test_post', 'group': self.group.id})
+        self.assertTrue(Post.objects.filter(text='test_post'))
+
+    def test_new_post_index_exist(self):
+        """При публикации поста он появится на главной странице (index)"""
+        response = self.client.get('')
+        self.assertContains(response, 'New_post')
+
+    def test_new_post_profile_exist(self):
+        """При публикации поста он появится на странице профайла пользователя (profile)"""
+        response = self.client.get(reverse('profile', kwargs={'username': self.user}))
+        self.assertContains(response, 'New_post')
+
+    def test_new_post_view_exist(self):
+        """При публикации поста он появится на странице поста (post)"""
+        post = Post.objects.last()
+        response = self.client.get(reverse('post', kwargs={'username': self.user, 'post_id': post.id}))
+        self.assertContains(response, 'New_post')
+
+    def test_new_post_group_exist(self):
+        """При публикации поста он появится на странице группы (group_posts)"""
+        response = self.client.get(reverse('group_posts', kwargs={'slug': self.group.slug}))
+        self.assertContains(response, 'New_post')
+
+    def test_guest_redirect_from_new_post(self):
+        """Гость не может создавать посты, его перенаправляет на страницу авторизации"""
+        self.client.logout()
+        response = self.client.get(reverse('new_post'))
+        self.assertRedirects(response, '/auth/login/?next=/new/')
+
+
+class CommentTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', email='q@q.com', password='12345')
+        self.author = User.objects.create_user(username='test_author', email='w@w.com', password='12345')
+        self.post = Post.objects.create(text='testtext', author=self.author)
+        self.client.login(username='testuser', password='12345')
+
+    def test_user_could_add_comment(self):
+        """Авторизированный пользователь может комментировать посты"""
+        self.client.post(
+            reverse('add_comment', kwargs={'username': self.author, 'post_id': self.post.id}),
+            data={'text': 'comment_text'}
+        )
+        response = self.client.get(reverse('post', kwargs={'username': self.author, 'post_id': self.post.id}))
+        self.assertContains(response, 'comment_text')
+
+    def test_guest_could_not_add_comment(self):
+        """Гость не может комментировать посты, его перенаправляет на страницу авторизации """
+        self.client.logout()
+        response = self.client.get(
+            reverse('add_comment', kwargs={'username': self.author, 'post_id': self.post.id}),
+            follow=True
+        )
+        self.assertTemplateUsed(response, 'registration/login.html')
+        self.assertEqual(
+            response.redirect_chain,
+            [('/auth/login/?next={}'.format(reverse(
+                'add_comment', kwargs={'username': self.author, 'post_id': self.post.id})), 302)]
+        )
+
+
+class FollowTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', email='q@q.com', password='12345')
+        self.author = User.objects.create_user(username='test_author', email='w@w.com', password='12345')
+        self.client.login(username='testuser', password='12345')
+
+    def test_auth_user_could_follow(self):
+        """Авторизированный пользователь может создать связь в бд с другим пользователем (подписаться)"""
+        self.assertEqual(Follow.objects.all().count(), 0)
+        response = self.client.get(reverse('profile_follow', kwargs={'username': self.author.username}), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Follow.objects.all().count(), 1)
+
+    def test_auth_user_could_unfollow(self):
+        """Авторизированный пользователь может разорвать связь в бд с другим пользователем (отписаться)"""
+        self.client.get(reverse('profile_follow', kwargs={'username': self.author.username}))
+        self.assertEqual(Follow.objects.all().count(), 1)
+        response = self.client.get(reverse('profile_unfollow', kwargs={'username': self.author.username}), follow=True)
+        self.assertEqual(Follow.objects.all().count(), 0)
+        self.assertEqual(response.status_code, 200)
+
+    def test_followed_user_could_see_post(self):
+        """Подписанный пользователь видит пост автора в ленте"""
+        self.client.get(reverse('profile_follow', kwargs={'username': self.author.username}))
+        post = Post.objects.create(text='testtext', author=self.author)
+        response = self.client.get(reverse('follow_index'))
+        self.assertContains(response, post.text)
+
+    def test_unfollowed_user_could_not_see_post(self):
+        """Не подписанный пользователь не видит пост автора в ленте"""
+        post = Post.objects.create(text='testtext', author=self.author)
+        response = self.client.get(reverse('follow_index'))
+        self.assertNotContains(response, post.text)
 
 
 class NewImageTest(TestCase):
@@ -87,7 +193,6 @@ class EditImageTest(TestCase):
         self.assertContains(response, '<img')
 
     def test_edit_image_profile(self):
-
         """После добавления изображения к посту, на странице пользователя (profile) есть тег <img>"""
         response = self.client.get(f"/author/{self.user.username}/")
         self.assertContains(response, '<img')
@@ -104,7 +209,7 @@ class EditImageTest(TestCase):
         self.assertContains(response, '<img')
 
 
-class UserPagesTest(TestCase):
+class ProfileTest(TestCase):
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user(username='testuser', email='q@q.com', password='12345')
@@ -114,52 +219,12 @@ class UserPagesTest(TestCase):
         response = self.client.get(reverse('profile', kwargs={'username': self.user.username}))
         self.assertEqual(response.status_code, 200)
 
-    def test_auth_user_new_post(self):
-        group = Group.objects.create(
-            title='test',
-            slug='test',
-            description='empty',
-        )
-        group_id = group.id
 
-        response = self.client.get(reverse('new_post'))
-        self.assertEqual(response.status_code, 200)
-        self.client.post(
-            reverse('new_post'),
-            data={
-                'text': 'text',
-                'group': group_id,
-            }
-        )
-        post = Post.objects.get(text='text', group=group_id)
-        self.assertTrue(post)
-        for url in ('', f'/author/{self.user.username}/', f'/author/{self.user.username}/{post.id}'):
-            response = self.client.get(url)
-            self.assertContains(response, post.text)
-        response = self.client.get('/author/{}/{}'.format(self.user.username, post.id))
-
-        self.assertEqual(response.status_code, 200)
-        response = self.client.get('/author/{}/{}/edit'.format(self.user.username, post.id))
-        self.assertEqual(response.status_code, 200)
-        self.client.post(
-            '/author/{}/{}/edit'.format(self.user.username, post.id),
-            data={
-                'text': 'edit text',
-                'group': group_id,
-            }
-        )
-        post = Post.objects.get(text='edit text', group=group_id)
-        self.assertTrue(post)
-
-        cache.clear()
-        for url in ('', f'/author/{self.user.username}/', f'/author/{self.user.username}/{post.id}'):
-            response = self.client.get(url)
-            self.assertContains(response, post.text)
-
-    def test_guest_redirect_from_new_post(self):
-        self.client.logout()
-        response = self.client.get('/new/')
-        self.assertRedirects(response, '/auth/login/?next=/new/')
+class ErrorTest(TestCase):
+    def test_404_error(self):
+        response = self.client.get('fgjsfg')
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateUsed(response, template_name='misc/404.html')
 
 
 class CacheTest(TestCase):
